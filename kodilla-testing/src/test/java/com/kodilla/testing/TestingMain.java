3ef4c4241fc77e5d38e8d9be46ca145d658e350c
naros1 <sorankasim@gmail.com>
2019-07-11 20:03:21 +0200

    First Commit

--- kodilla-testing/src/test/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,19 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator(3,5);
+
+        int resultAdd = calculator.getA() + calculator.getB();
+        int resultSubtract = calculator.getA() - calculator.getB();
+
+        if (calculator.add() == resultAdd && calculator.subtract() == resultSubtract){
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
