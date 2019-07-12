66f84e8790065b39b9f5147f7d1b67939dd31c27
naros1 <sorankasim@gmail.com>
2019-07-12 19:37:07 +0200

    first commit

--- kodilla-testing/src/test/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/TestingMain.java
@@ -5,6 +5,18 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
+        Calculator calculator = new Calculator();
+
+        int toA = 5;
+        int toB = 3;
+        int resultAdd =  toA + toB;
+        int resultSubtract = toA -toB;
+
+        if(calculator.add(toA,toB) == resultAdd && calculator.subtract(toA, toB) == resultSubtract){
+            System.out.println("Test Ok");
+        }else {
+            System.out.println("Error");
+        }
 
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/calculator/Calculator.java
@@ -4,24 +4,16 @@ public class Calculator {
     int a;
     int b;
 
-    public Calculator(int a, int b){
-        this.a = a;
-        this.b = b;
+    public Calculator(){
+
     }
 
-    public int add(){
-        return a + b;
+    public int add(int toA, int toB){
+
+        return toA + toB;
     }
 
-    public int subtract(){
-        return a - b;
-    }
-
-    public int getA(){
-        return a;
-    }
-
-    public int getB(){
-        return b;
+    public int subtract(int toA, int toB){
+        return toA - toB;
     }
 }
