777a8aaf977fd9b29aa37453a211b4e85ba736be
naros1 <sorankasim@gmail.com>
2019-07-21 22:40:25 +0200

    first commit

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
+
+    static double getAverage(int[] numbers){
+        int length = numbers.length;
+       IntStream.range(0, length)
+               .forEach(System.out::println);
+
+
+
+        double average = IntStream.range(0, length)
+                .map(n -> numbers[n])
+                .average().getAsDouble();
+
+        return average;
+
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,30 @@
 package com.kodilla.stream.array;
 
-public class ArrayOperationsTestSuite {
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.OptionalDouble;
+
+public class ArrayOperationsTestSuite implements ArrayOperations {
+    @Test
+    public void testGetAverage() {
+        //Given
+        int numbers[] = new int[20];
+        for (int i = 0; i < 20 ; i++) {
+            numbers[i] = i ;
+        }
+        int sum = 0;
+        for (int i = 0; i < 20 ; i++){
+            sum = sum + numbers[i];
+        }
+
+        double averageByAssertCalculated = (double) sum/ (double) numbers.length;
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+
+        //Then
+
+        Assert.assertEquals(averageByAssertCalculated, average, 0.001);
+    }
 }
