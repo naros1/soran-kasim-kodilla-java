8b3b3c942e90834a25c08efb6d24263972a35cc0
naros1 <sorankasim@gmail.com>
2019-07-25 23:42:01 +0200

    first commit

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -0,0 +1,11 @@
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FieldReader;
+
+public class ExceptionModuleRunner {
+    public static void main(String[] args){
+
+        FieldReader fieldReader = new FieldReader();
+        fieldReader.readFile();
+    }
+}
