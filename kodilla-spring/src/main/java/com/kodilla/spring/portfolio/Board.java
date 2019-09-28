a2bfc750287cf42ea7097d1ffa5eb403949fccda
naros1 <sorankasim@gmail.com>
2019-09-28 13:29:16 +0200

    first commit

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -1,9 +1,9 @@
 package com.kodilla.spring.portfolio;
 
 public final class Board {
-    final TaskList toDoList;
-    final TaskList inProgressList;
-    final TaskList doneList;
+    private final TaskList toDoList;
+    private final TaskList inProgressList;
+    private final TaskList doneList;
 
     public Board(final TaskList toDoList,final TaskList inProgressList,final TaskList doneList) {
         this.toDoList = toDoList;
