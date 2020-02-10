d36c2eca4b9899c905e7de81e0f82cc36d0b33e1
naros1 <sorankasim@gmail.com>
2020-02-10 23:48:22 +0100

    git commit -m "first commit"

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -11,11 +11,11 @@ public class DbManager {
 
     private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
-        connectionProps.put("user", "kodilla_user");
-        connectionProps.put("password", "kodilla_password");
+        connectionProps.put("user", "kodilla");
+        connectionProps.put("password", "19584907");
+        //Class.forName("com.mysql.jdbc.Driver");
         conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +
-                        "&useSSL=False",
+                "jdbc:mysql://localhost:3306/kodilla_project?serverTimezone=Europe/Warsaw"+"&useSSL=False",
                 connectionProps);
     }
 
